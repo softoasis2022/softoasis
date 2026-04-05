9d1bb5a259a76c8e54a468b0a147c53cb3e3d88a
softoasis <hang05312@softoasis.org>
2026-04-05 19:59:27 +0900

    add missed files

--- app/softoasis/app.js
+++ app/softoasis/app.js
@@ -1,5 +1,5 @@
-const express = require("express"); 
-const path = require("path"); 
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
 const routes = express.Router();
 
@@ -11,6 +11,33 @@ const imgDB = path.join(database, "image");
 const TEMPLATE_PATH = path.join(PAGES_DIR, "tamplate", "index.html");
 
 routes.use("/pages", express.static(path.join(__dirname, "pages")));
+routes.use("/css", express.static(path.join(__dirname, "pages")));
+routes.use("/js", express.static(path.join(__dirname, "pages")));
+
+routes.get("/", (req, res) => {
+    const pagePath = path.join(PAGES_DIR, "main", "index.html");
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
 
 
+/**
+ * 템플릿 렌더링
+ */
+function renderTemplate(pagePath) {
+    const templatePath = path.join(TEMPLATE_PATH);
+
+    try {
+        let template = fs.readFileSync(templatePath, "utf-8");
+        const pageContent = fs.readFileSync(pagePath, "utf-8");
+
+        return template.replace("<!-- MAIN_CONTENT -->", pageContent);
+    } catch (err) {
+        console.error("템플릿 렌더링 실패:", err);
+        return null;
+    }
+}
 module.exports = routes;
