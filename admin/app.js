980442af1d4591fadda0a35c338589382d632263
softoasis <hang05312@softoasis.org>
2026-04-08 00:49:14 +0900

    add missed files

--- admin/app.js
+++ admin/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const routes = express.Router();
 
-const PAGES_DIR = path.join(__dirname, "routes");
+const PAGES_DIR = path.join(__dirname, "pages");
 const TEMPLATE_DIR = path.join(__dirname, "routes", "tamplate", "html");
 routes.use("/css", express.static(path.join(__dirname, "routes", "tamplate", "style")));
 routes.use("/js", express.static(path.join(__dirname, "routes", "tamplate", "script")));
@@ -11,17 +11,44 @@ routes.use("/js", express.static(path.join(__dirname, "routes", "tamplate", "scr
 // 정적 파일 제공
 routes.use("/pages", express.static(PAGES_DIR));
 
-const mobileroutes = require("./routes/app"); 
-const HRroutes = require("./routes/HR/app");
-const userroutes = require("./routes/user/app");
-const approutes =  require("./routes/main/main");
+const mobileroutes = require("./routes/mobile"); 
+const HRroutes = require("./routes/HR");
+const userroutes = require("./routes/user");
+const approutes =  require("./routes/home");
 routes.use("/mobile", mobileroutes);
 routes.use("/HR", HRroutes);
+routes.use("/home", approutes);
 routes.use("/user", userroutes);
-routes.use("/", approutes);
-// 기본 페이지 (root 요청)
+
+
+
+
 routes.get("/", (req, res) => {
-    const pagePath = path.join(PAGES_DIR, "main", "pages", "html", "index.html");
+    const pagePath = path.join(PAGES_DIR,"html","home","index.html");
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
+routes.get("/mobile", (req, res) => {
+    const pagePath = path.join(PAGES_DIR,"html","mobile","index.html");
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
+routes.get("/user", (req, res) => {
+    const pagePath = path.join(PAGES_DIR,"html","user","index.html");
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
+routes.get("/HR", (req, res) => {
+    const pagePath = path.join(PAGES_DIR,"html","home","HR.html");
 
     const result = renderTemplate(pagePath);
     if (!result) return res.status(500).send("템플릿 구성 중 오류");
