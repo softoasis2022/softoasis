be3964528611b06bb9974dbcb34fcc266b32f2f9
softoasis <hang05312@softoasis.org>
2026-04-09 03:45:22 +0900

    add missed files

--- admin/app.js
+++ admin/app.js
@@ -1,18 +1,64 @@
+//경로 admin.softoasis.org로 라우팅 되어 있음
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const routes = express.Router();
 
-
-
 const mobileroutes = require("./routes/mobile"); 
 const HRroutes = require("./routes/HR");
 const userroutes = require("./routes/user");
 const approutes =  require("./routes/home");
+const acountroutes =  require("./routes/acount");
+
+routes.use("/acount", acountroutes);
+
+//쿠키에 adminid 가 없으면 로그인 페이지(경로 /acount로 리디렉션 : 클라이언트는 admim.softoasis.org/acount/login 으로 리디렉션됨)
+//
+//
+// routes.use((req, res, next) => {
+
+//     // 로그인 관련은 통과
+//     if (req.path.startsWith("/acount")) {
+//         return next();
+//     }
+
+//     const cookies = parseCookies(req.headers.cookie);
+//     const adminId = cookies.adminid;
+
+//     if (!adminId) {
+//         return res.redirect("/acount/login");
+//     }
+
+//     const sessionFile = path.join("C:", "database", "session", `${adminId}.json`);
+
+//     if (!fs.existsSync(sessionFile)) {
+//         return res.redirect("/acount/login");
+//     }
+
+//     req.admin = JSON.parse(fs.readFileSync(sessionFile, "utf-8"));
+
+//     next();
+// });
+
+function parseCookies(cookieStr) {
+    const cookies = {};
+    if (!cookieStr) return cookies;
+
+    cookieStr.split(";").forEach(cookie => {
+        const [key, value] = cookie.trim().split("=");
+        cookies[key] = value;
+    });
+
+    return cookies;
+}
+
+
 routes.use("/mobile", mobileroutes);
 routes.use("/HR", HRroutes);
 routes.use("/home", approutes);
 routes.use("/", approutes);
 routes.use("/user", userroutes);
+
 
 module.exports = routes;
--- admin/routes/HR/task.js
+++ admin/routes/HR/task.js
@@ -26,6 +26,32 @@ routes.get("/input", (req, res) => {
 
     res.send(result);
 });
+routes.post("/input", (req, res) => {
+    //업무분류("긴급","기존업무 연게",),업무이름,담당부서,담당자,전달부서(업무후 전달할 부서 : 전달부서가 없어도됨),업무내용
+    //서버 기준: 업무등록 시간[업무등록시간은 서버 시간 기준으로 저장], 업무번호 부여[업무 번호는 "./task.json"에서 데이터를 가져와서 json데이터에 number를 +1올리고 가져온 넘버를 업무번호로 부여]
+    //업무상태(키 : state)는 새로등록이기때문에 무조건 "등록"이다
+
+    const { name, }= req.body;
+    const inputPath = path.join(database, "task");
+
+    //inputPath에 저장
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
+//  경로 /update  post
+routes.post("/update/:action",(req,res)=>{
+    //업무
+    // 업무 이동 타임 라인(리스트형식 : 업무 이름,)
+    //action 은 업무이동(루트 아직 못정함),업무상태변경(루트 아직 못정함 : ),
+    //업무 수신자(해당업무를 바톤터치 하는 )
+    //모든 서버요청의 요청 데이터는 요청자를 기준으로 한다. 업무담당자와 업데이트요청자가 다를수 있음
+    const { action } = req.params;
+    const {tasknumber,memberid} = req.body
+});
+
 
 function renderTemplate(pagePath) {
     const templatePath = path.join(TEMPLATE_DIR);
