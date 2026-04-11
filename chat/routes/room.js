18a3762ab4303ecdfeeb1b078446c4606b84ae61
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-04-12 00:17:15 +0900

    add missed files

--- chat/routes/room.js
+++ chat/routes/room.js
@@ -9,18 +9,25 @@ routes.use("/css",express.static(path.join(__dirname,"../pages/room")));
 
 //경로 /room 라우팅 예정
 
+
+routes.use((req,res,next)=>{
+    //
+    next();
+});
+
 // 파트너스,노드,컨텐트 
 routes.get("/", (req, res) => {
-    
+    const {roomnumber, usernumber} = req.query;
 });
 routes.get("/partners", (req, res) => {
-
+    const {roomnumber, usernumber} = req.query;
+    
 });
 routes.get("/node", (req, res) => {
-
+    const {roomnumber, usernumber} = req.query;
 });
 routes.get("/content", (req, res) => {
-
+    const {roomnumber, usernumber} = req.query;
 });
 
 module.exports =routes;
