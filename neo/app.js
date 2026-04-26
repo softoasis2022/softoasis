ec3c40d5450ee900dd8545c8ac3d956931b077b4
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-04-27 03:07:47 +0900

    add missed files

--- neo/app.js
+++ neo/app.js
@@ -22,6 +22,7 @@ routes.use("/contant",contant);
 routes.use("/recipe", require("./routes/recipe/app"));
 //neo에서 사용하는 메인 컨텐츠 neo.softoasis.org, 제공 제한 : 없음 (로그인이 필요한 것들은 제한, 개별 로그인 사용)
 routes.use("/world", require("./routes/world/app"));
+routes.use("/tester", require("./routes/tester/app"));
 
 routes.post("/join",(req,res)=>{
     const {roomnumber} = req.body;
