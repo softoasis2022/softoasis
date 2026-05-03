e62497b53cd9420a3ddffbf841b10d8deb099283
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-05-04 04:01:50 +0900

    add missed files

--- neo/app.js
+++ neo/app.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 //neo.softoasis.org 라우팅 완료
 
 const social = require("./routes/social/social");
-const contant = require("./routes/contant/app");
+const content = require("./routes/content/app");
 const intropageroute = require("./routes/intro/app");
 const mainpageroute = require("./routes/main/app");
 const tamplatepageroute = require("./routes/tamplate/app");
@@ -18,7 +18,7 @@ routes.use("/social",social);
 routes.use("/",mainpageroute);
 routes.use("/main",mainpageroute);
 routes.use("/intro",intropageroute);
-routes.use("/contant",contant);
+routes.use("/content",content);
 routes.use("/recipe", require("./routes/recipe/app"));
 //neo에서 사용하는 메인 컨텐츠 neo.softoasis.org, 제공 제한 : 없음 (로그인이 필요한 것들은 제한, 개별 로그인 사용)
 routes.use("/world", require("./routes/world/app"));
