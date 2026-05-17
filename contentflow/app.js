603d90f33a00c1468b41de65b369c42e7d534d11
softoasis2022 <132141967+softoasis2022@users.noreply.github.com>
2026-05-18 05:35:41 +0900

    add missed files

--- contentflow/app.js
+++ contentflow/app.js
@@ -46,6 +46,14 @@ routes.get("/contentinfo", (req, res) => {
 
     res.send(result);
 });
+routes.get("/search", (req, res) => {
+    const pagePath = path.join(PAGES_DIR,"html", "search.html");
+
+    const result = renderTemplate(pagePath);
+    if (!result) return res.status(500).send("템플릿 구성 중 오류");
+
+    res.send(result);
+});
 routes.get("/payment", (req, res) => {
     const pagePath = path.join(PAGES_DIR,"html", "payment.html");
 
